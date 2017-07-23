683a000334ac5985c45f16122b4c79688ed58150
Oscmage <oscar.evertsson@live.com>
2017-07-23 16:17:44 +0200

    Added border-box.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { injectGlobal } from "styled-components";
+// eslint-disable-next-line
+injectGlobal`
+    *, *:before, *:after {
+        box-sizing: border-box;
+    }
+
+    body {
+        margin: 0;
+        padding-bottom: 2rem;
+    }
+`;
 
 ReactDOM.render(<App />, document.getElementById("root"));
